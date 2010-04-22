95a5d02f3c834d720b2f78fa3532b4f18cc3d0d3
Chris Wise <cwise@murmurinformatics.com>
2010-04-22 06:29:50 -0400

    changed method of game responses

--- config/environment.rb
+++ config/environment.rb
@@ -5,15 +5,6 @@ require "action_mailer"
 
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
-
-  config.action_mailer.delivery_method = :sendmail
-  ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => "mailer@murmurinformatics.com",
-    :password => "n3v3rspam",
-    :authentication => :login
-  }
 end
 
 require "will_paginate"
