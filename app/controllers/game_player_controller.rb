f5b8d81f810d63b06112434c8449f5868513e2ce
Chris Wise <cwise@murmurinformatics.com>
2010-08-24 17:38:28 -0400

    changed cookie method to rails 2.3.5 way

--- app/controllers/game_player_controller.rb
+++ app/controllers/game_player_controller.rb
@@ -5,7 +5,7 @@ class GamePlayerController < ApplicationController
 
     if request.post?
       @game_player=GamePlayer.new(params[:game_player])
-      cookies.permanent[:email_address]={ :value => @game_player.email_address, :expires => 1.year.from_now }
+      cookies[:email_address]={ :value => @game_player.email_address, :expires => 1.year.from_now }
       player=Player.find_by_email_address(@game_player.email_address)
       if player.nil?
         @game_player.errors.add("Player not found by address") 
