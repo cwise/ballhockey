46efcc6518296478dc5595ebdab8bc597eb3c6a1
Chris Wise <cwise@murmurinformatics.com>
2010-10-15 07:21:38 -0400

    added missing gems

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,8 @@ require "action_mailer"
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
   config.gem "will_paginate", :version => '2.3.14'
+  config.gem "whenever"
+  config.gem "backup"
 end
 
 require "will_paginate"
