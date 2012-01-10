ddc304844e5740363f6b3433b9467f24afac28ab
Chris Wise <cwise@murmurinformatics.com>
2012-01-10 10:31:57 -0500

    added deprecation notice config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,11 +6,15 @@ Nrcanhockey::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  # Specifies the header that your server uses for sending files
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+
   # Full error reports are disabled and caching is turned on
   config.action_controller.consider_all_requests_local = false
   config.action_controller.perform_caching             = true
   config.action_view.cache_template_loading            = true
-
+  config.active_support.deprecation = :notify
+  
   config.action_mailer.default_url_options = { :host => "nrcanhockey.murmurinfo.ca" }
   #config.action_mailer.delivery_method = :sendmail
   #ActionMailer::Base.smtp_settings = {
