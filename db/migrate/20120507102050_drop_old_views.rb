27df435919625ccb2adf4a7ef6f457a619ffed70
Chris Wise <cwise@murmurinformatics.com>
2012-10-26 20:08:34 -0400

    disabled migration

--- db/migrate/20120507102050_drop_old_views.rb
+++ db/migrate/20120507102050_drop_old_views.rb
@@ -1,7 +1,7 @@
 class DropOldViews < ActiveRecord::Migration
   def self.up
-    drop_table :played_games
-    drop_table :game_goalies
+  #  drop_table :played_games
+  #  drop_table :game_goalies
   end
 
   def self.down
