47dac656bff345582cc418a56321f237d2809ece
Chris Wise <cwise@murmurinformatics.com>
2013-04-07 13:35:10 -0400

    added code to avoid Resque forking issues

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,10 @@
 unless $heroku_deploying
 	uri = URI.parse(ENV["REDISTOGO_URL"])
 	Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) 
+	
+	Resque.after_fork do
+    Resque.redis.client.reconnect
+  end
 else
   puts "Skipping Resque configuration due to Heroku precompilation"
 end
--- lib/tasks/resque.rake
+++ lib/tasks/resque.rake
@@ -1,3 +1,7 @@
 require "resque/tasks"  
   
 task "resque:setup" => :environment
+
+Resque.after_fork do |job|
+  ActiveRecord::Base.establish_connection
+end
