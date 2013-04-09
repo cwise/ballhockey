29e406b004960b89e3fa3e06a071346462d248d5
Chris Wise <cwise@murmurinformatics.com>
2013-04-09 10:45:18 -0400

    made sure delivery errors raised in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ BallHockey::Application.configure do
   config.assets.compile = true
   config.assets.digest = true  
   
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
