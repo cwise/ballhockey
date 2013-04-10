285668b35b3cd1be845e0092c164a813743e825f
Chris Wise <cwise@murmurinformatics.com>
2013-04-10 08:13:25 -0400

    added missing mail interceptor to sendgrid

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -8,6 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 
+ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
+
 BallHockey::Application.configure do
   config.action_mailer.default_url_options = { :host => ENV['DEFAULT_HOST'] }
 
