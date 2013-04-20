055cc799e57e1f342d9cfac467ed057191d0f2f8
Chris Wise <cwise@murmurinformatics.com>
2013-04-20 19:26:07 -0400

    updated HireFire patch to use Heroku-API

--- config/initializers/patches/heroku_cedar_hirefire_patch.rb
+++ config/initializers/patches/heroku_cedar_hirefire_patch.rb
@@ -1,45 +1,45 @@
-# hopefully a temporary patch so HireFire will run on the Heroku Cedar stack
-# NB: the worker type is hard-coded as "worker" below, this must correlate to the type in Procfile
-# NB: ENV['APP_NAME'] must be defined (e.g. 'heroku config:add APP_NAME=myherokuappname')
- 
-# using ps & ps_scale instead of info & set_workers
-class HireFire::Environment::Heroku
-  
-  private
- 
-  def workers(amount = nil)
-    
-    if amount.nil?
-      # return client.info(ENV['APP_NAME'])[:workers].to_i
-      return client.ps(ENV['APP_NAME']).select {|p| p['process'] =~ /worker.[0-9]+/}.length
-    end
- 
-    # client.set_workers(ENV['APP_NAME'], amount)
-    return client.ps_scale(ENV['APP_NAME'], {"type" => "worker", "qty" => amount})
- 
-  rescue RestClient::Exception
-    HireFire::Logger.message("Worker query request failed with #{ $!.class.name } #{ $!.message }")
-    nil
-  end
-end
- 
-# quick override to ensure that HireFire is activated on Heroku. The change is that it is looking for
-# ::Rails.env.production? instead of ENV.include?('HEROKU_UPID')
+require 'heroku-api'
 module HireFire
   module Environment
- 
-    module ClassMethods
-      
-      def environment
-        @environment ||= HireFire::Environment.const_get(
-          if environment = HireFire.configuration.environment
-            environment.to_s.camelize
-          else
-            ::Rails.env.production? ? 'Heroku' : 'Noop'
-          end
-        ).new
+    class Heroku < Base
+
+      private
+
+      ##
+      # Either retrieves the amount of currently running workers,
+      # or set the amount of workers to a specific amount by providing a value
+      #
+      # @overload workers(amount = nil)
+      #   @param [Fixnum] amount will tell heroku to run N workers
+      #   @return [nil]
+      # @overload workers(amount = nil)
+      #   @param [nil] amount
+      #   @return [Fixnum] will request the amount of currently running workers from Heroku
+      def workers(amount = nil)
+
+        #
+        # Returns the amount of Delayed Job
+        # workers that are currently running on Heroku
+        if amount.nil?
+          return heroku.get_ps(ENV['APP_NAME']).body.select {|p| p['process'] =~ /worker.[0-9]+/}.length
+        end
+
+        ##
+        # Sets the amount of Delayed Job
+        # workers that need to be running on Heroku
+        return heroku.post_ps_scale(ENV['APP_NAME'], "worker", amount)
+
+      rescue ::Heroku::API::Errors::Error
+        HireFire::Logger.message("Worker query request failed with #{ $!.class.name } #{ $!.message }")
+        nil
       end
-      
+
+      ##
+      # Get the heroku connection. Needs to have the HEROKU_API_KEY in the environment
+      def heroku(api_key = nil)
+        @heroku ||= ::Heroku::API.new(:api_key => api_key || ENV['HEROKU_API_KEY'])
+      end
+
     end
   end
 end
