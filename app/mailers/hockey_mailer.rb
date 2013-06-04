af22305bb1ef797c521f0010a5689c94d3584438
Chris Wise <cwise@murmurinformatics.com>
2013-06-04 06:17:22 -0400

    removed kill stale workers code

--- app/mailers/hockey_mailer.rb
+++ app/mailers/hockey_mailer.rb
@@ -3,35 +3,30 @@ class HockeyMailer < ActionMailer::Base
   default :from => ENV['MAIL_SENDER']  
   
   def announce_game game_id
-    kill_stale_workers
     load_game game_id
     uniq_args({ :type => 'game', :id => @game.id })    
     mail base_options.merge({ :subject => "#{ENV['LEAGUE_NAME']} Ball Hockey Game for #{@game.game_date}?"})
   end
 
   def update_game game_id
-    kill_stale_workers
     load_game game_id
     uniq_args({ :type => 'game', :id => @game.id })    
     mail base_options.merge({ :subject => "#{ENV['LEAGUE_NAME']} Ball Hockey Game Update for #{@game.game_date}"})
   end
 
   def call_game game_id
-    kill_stale_workers
     load_game game_id
     uniq_args({ :type => 'game', :id => @game.id })    
     mail base_options.merge({ :subject => "#{ENV['LEAGUE_NAME']} Ball Hockey Game for #{@game.game_date}: Game ON!"})
   end
 
   def cancel_game game_id
-    kill_stale_workers
     load_game game_id
     uniq_args({ :type => 'game', :id => @game.id })    
     mail base_options.merge({ :subject => "#{ENV['LEAGUE_NAME']} Ball Hockey Game for #{@game.game_date}: Game Cancelled"})
   end
   
   def test to
-    kill_stale_workers
     mail :to => to, :content_type => "text/html", :subject => 'Test Message'
   end
   
@@ -42,9 +37,5 @@ class HockeyMailer < ActionMailer::Base
   
   def load_game game_id
     @game = Game.find game_id
-  end
-  
-  def kill_stale_workers
-    Resque.workers.each{ |w| w.unregister_worker }
   end
 end
